7c3468f49263159db8f8b4e271d5d18415e50303
Fabricio Correa <faerroc@gmail.com>
2020-06-20 17:25:49 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//=====================================================================================
+// main.cpp
+//
+// This is a simple C++ project using cmake
+//
+// Date       Autor             Obs
+// 20-06-2020 Fabricio Correa   Initial version
+//
+// References
+// http://derekmolloy.ie/hello-world-introductions-to-cmake/
+// https://mirkokiefer.com/cmake-by-example-f95eb47d45b1
+//=====================================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+   cout << "Hello World !" << endl;
+   
+   return 0;
+}
